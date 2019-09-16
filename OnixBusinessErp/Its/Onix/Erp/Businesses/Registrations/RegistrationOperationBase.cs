cf6060139cc2daebe0c617a6856079e4a6b88207
pjamenaja <pjame.fb@gmail.com>
2019-09-16 22:05:18 +0700

    Removed duplicate code

---
+++ OnixBusinessErp/Its/Onix/Erp/Businesses/Registrations/RegistrationOperationBase.cs
@@ -0,0 +1,65 @@
+using System;
+
+using Microsoft.Extensions.Logging;
+
+using Its.Onix.Erp.Models;
+using Its.Onix.Core.Business;
+using Its.Onix.Erp.Utils;
+using Its.Onix.Core.Utils;
+
+namespace Its.Onix.Erp.Businesses.Registrations
+{
+    public abstract class RegistrationOperationBase : BusinessOperationBase, IBusinessOperationManipulate<MRegistration>
+    {
+        protected abstract void ValidateRegistration(MRegistration dat);
+        protected abstract void PerformRegistrationAction(MRegistration dat, string barcode);
+        protected abstract void ValidateActivation(MRegistration dat, MBarcode bc, string barcode);
+
+        public int Apply(MRegistration dat)
+        {
+            ILogger logger = GetLogger();
+
+            ValidateRegistration(dat);
+
+            string barcode = string.Format("{0}-{1}", dat.SerialNumber, dat.Pin);
+            MBarcode bc = null;
+            string bcPath = null;
+            var ctx = GetNoSqlContext();
+
+            if ((dat.SerialNumber.Length > 3) && (dat.Pin.Length > 3))
+            {
+                bcPath = BarcodeUtils.BuildBarcodePath("barcodes", dat.SerialNumber, dat.Pin);
+                bc = ctx.GetObjectByKey<MBarcode>(bcPath);
+            }
+
+            string path = "";
+            if (bc == null)
+            {
+                dat.Status = "NOTFOUND";
+                path = string.Format("registrations/{0}/{1}", dat.Status, barcode);
+                ctx.PostData(path, dat);
+
+                string msg = string.Format("Serial number and PIN not found [{0}]", barcode);
+                LogUtils.LogInformation(logger, msg);
+
+                throw (new ArgumentException(msg));
+            }
+
+            dat.RegistrationDate = DateTime.Now;
+            dat.LastMaintDate = DateTime.Now;            
+
+            ValidateActivation(dat, bc, barcode);
+            
+            //Update status back to barcode
+
+            bc.IsActivated = true;
+            bc.ActivatedDate = DateTime.Now;
+            bc.LastMaintDate = DateTime.Now;
+            ctx.PutData(bcPath, bc.Key, bc);
+
+            PerformRegistrationAction(dat, barcode);
+
+            return 0;
+        }
+    }
+}
